f4de092762137fda0818cd99d1b7f9415082cf5b
qukato <qukato@users.noreply.github.com>
2025-07-08 11:28:23 +0000

    SSSD qukato/blog-main@6513d4786099502345a9e73c1508111cdad0516d

--- README.md
+++
@@ -1,2 +0,0 @@
-# qukato.github.io
-blogweb
---
+++ searchindex.en.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
